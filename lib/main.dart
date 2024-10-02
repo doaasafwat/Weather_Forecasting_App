2991458a31304c3bb6c03fda1572d5c5967551dd
doaasafwat <doaasafwat54@gmail>
2024-10-02 21:21:43 +0300

    create project_files

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherForeCast());
+}
+
+class WeatherForeCast extends StatelessWidget {
+  const WeatherForeCast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
