4ed217093261eb7f5717b279a61a10e61ba27557
doaasafwat <doaasafwat54@gmail>
2024-10-15 17:19:18 +0300

    /

--- lib/views/weather_today_screen.dart
+++ lib/views/weather_today_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_forecasting_app/cubits/get_weather_cubit/get_weather_cubit.dart';
@@ -21,152 +22,154 @@ class WeatherTodayScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF1D2837),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder:(context) => const SearchView(),));
-          },
-        ),
-        centerTitle: true,
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Icon(Icons.location_pin, color: Colors.purpleAccent),
-            const SizedBox(width: 5),
-            Text(
-              weatherModel.location.name,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            const Icon(Icons.arrow_drop_down, color: Colors.white),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Image.network(
-                    'https:${weatherModel.current.iconUrl}',
-                    height: 100,
-                    width: 100,
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    weatherModel.current.condition,
-                    style: const TextStyle(
-                      fontSize: 34,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    '${weatherModel.current.temperature}°',
-                    style: const TextStyle(
-                      fontSize: 52,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 22),
-            // Stats Row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                WeatherStat(
-                  unit: '%',
-                  value: '${weatherModel.current.humidity}',
-                  color: Colors.purple,
+    try {
+      return Scaffold(
+        backgroundColor: const Color(0xFF1D2837),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const  SearchView(),));
+            },
+          ),
+          centerTitle: true,
+          title: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.location_pin, color: Colors.purpleAccent),
+              const SizedBox(width: 5),
+              Text(
+                weatherModel.location.name,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
                 ),
-                WeatherStat(
+              ),
+              const Icon(Icons.arrow_drop_down, color: Colors.white),
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https:${weatherModel.current.iconUrl}',
+                      height: 100,
+                      width: 200,
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      weatherModel.current.condition,
+                      style: const TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${weatherModel.current.temperature}°',
+                      style: const TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 22),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  WeatherStat(
                     unit: '%',
                     value: '${weatherModel.current.humidity}',
-                    color: Colors.purple),
-                WeatherStat(
-                    unit: 'Km/h',
-                    value: '${weatherModel.current.windSpeed}',
-                    color: Colors.purple)
-              ],
-            ),
-            const SizedBox(height: 22),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Today',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
+                    color: Colors.purple,
+                  ),
+                  WeatherStat(
+                      unit: '%',
+                      value: '${weatherModel.current.humidity}',
+                      color: Colors.purple),
+                  WeatherStat(
+                      unit: 'Km/h',
+                      value: '${weatherModel.current.windSpeed}',
+                      color: Colors.purple)
+                ],
+              ),
+              const SizedBox(height: 22),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Today',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          context
-                              .read<GetWeatherCubit>()
-                              .getWeather(cityName: weatherModel.location.name);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => WeatherForecastScreen(
-                                cityName: weatherModel.location.name,
-                                weatherModel: weatherModel,
+                        GestureDetector(
+                          onTap: () {
+                            context
+                                .read<GetWeatherCubit>()
+                                .getWeather(cityName: weatherModel.location.name);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => WeatherForecastScreen(
+                                  cityName: weatherModel.location.name,
+                                  weatherModel: weatherModel,
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                        child: const Center(
-                          child: Text(
-                            "7 Days",
-                            style: TextStyle(
-                              color: Colors.purpleAccent,
-                              fontSize: 20,
+                            );
+                          },
+                          child: const Center(
+                            child: Text(
+                              "7 Days",
+                              style: TextStyle(
+                                color: Colors.purpleAccent,
+                                fontSize: 20,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 22),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: weatherModel.forecast[0].hour.take(5).map((hour) {
-                      String time = hour.time.contains(' ')
-                          ? hour.time.split(' ')[1]
-                          : hour.time;
+                      ],
+                    ),
+                    const SizedBox(height: 22),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: weatherModel.forecast[0].hour.take(5).map((hour) {
+                        String time = hour.time.contains(' ')
+                            ? hour.time.split(' ')[1]
+                            : hour.time;
 
-                      return HourlyForecastItem(
-                        time: time,
-                        temp: '${hour.temperature}°',
-                        icon: getWeatherIcon(hour.condition),
-                      );
-                    }).toList(),
-                  )
-                ],
+                        return HourlyForecastItem(
+                          time: time,
+                          temp: '${hour.temperature}°',
+                          icon: getWeatherIcon(hour.condition),
+                        );
+                      }).toList(),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } catch (e) {
+      return Center(child: Text('Error: ${e.toString()}'));
+    }
   }
 }
